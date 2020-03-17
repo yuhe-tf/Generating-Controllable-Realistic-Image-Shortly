c9a6a15f69d130fd839484b9c292d848ed3cbb9f
yuhe-tf <58869413+yuhe-tf@users.noreply.github.com>
2020-03-17 19:29:12 +0800

    Update model.py

--- model.py
+++ model.py
@@ -113,60 +113,6 @@ def my_resblock_classifier(input_shape, n_classes, kernel_size=5, name='my_class
     return Model(inputs, outputs, name=name)
 
 
-'''
-
-
-def my_resblock_generator(input_shape, dim=32, kernel_size=3,
-                          norm='batch_norm', name='my_resnet_generator'):
-    inputs = tf.keras.Input(input_shape)
-    output = conv_util(inputs, dim, kernel_size=1)
-    output = resize_up(output, dim * 2, kernel_size=3, up_times=4)
-
-    up_times = 3
-    for up_time in range(up_times):
-        dim = dim * 2
-        output = res_block(output, dim, kernel_size, norm)
-        if up_time == up_times - 1:
-            output = resize_up(output, dim // 4, kernel_size=kernel_size)
-        else:
-            output = resize_up(output, dim * 2, kernel_size=kernel_size)
-
-    output = conv_util(output, dim, kernel_size=1)
-    output = conv_util(output, 3, kernel_size, norm='none', activation='none')
-    output = tf.keras.layers.Activation('tanh')(output)
-    return tf.keras.Model(inputs, output, name=name)
-
-
-def my_resblock_discriminator(input_shape, dim=32, kernel_size=3, norm='none', name='my_resnet_discriminator'):
-    inputs = tf.keras.Input(input_shape)
-    output = conv_util(inputs, filters=dim * 2, kernel_size=5, strides=1, padding='same', norm=norm)
-
-    down_times = 3
-    for down_time in range(down_times):
-        dim = dim * 2
-        output = res_block(output, dim, kernel_size, norm)
-        output = down_util(output)
-        if down_time == down_times - 1:
-            output = conv_util(output, filters=dim // 2, kernel_size=kernel_size, norm=norm)
-        else:
-            output = conv_util(output, dim * 2, kernel_size=kernel_size, norm=norm)
-
-    q_output = Conv2D(filters=1, kernel_size=4, strides=1, padding='valid')(output)
-    output_shape = output.shape
-    return Model(inputs, outputs=[q_output, output], name=name), output_shape
-
-
-def my_resblock_classifier(input_shape, n_classes, kernel_size=5, name='my_classifier_model'):
-    inputs = tf.keras.Input(input_shape)
-    dims = input_shape[-1]
-
-    output = res_block(inputs, filters=dims, kernel_size=kernel_size, norm='batch_norm')
-    output = down_util(output)
-
-    output = tf.keras.layers.GlobalAveragePooling2D()(output)
-    outputs = Dense(n_classes, activation='softmax')(output)
-    return Model(inputs, outputs, name=name)
-'''
 
 if __name__ == '__main__':
     model_, _ = my_resblock_discriminator((32, 32, 3))
